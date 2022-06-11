4ef7764726cf98170196ebfe3b243603d72c03a2
HarshKumar Jha <jhaharsh878@gmail.com>
2022-06-11 22:20:23 +0530

    made changes in package.json and public/index.html and public/manifest.json by adding origin url of vercel in order to deploy my website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin , faFacebook , faInstagram , faGithub , faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
@@ -14,32 +16,36 @@ const Footer = () => {
             <div className="social_media">
                <span>Social Media</span>
                <div className="text2">
-                  <a href="https://www.linkedin.com/in/adarsh-shahi-a2282a190/" rel="noreferrer"
-                     target="_blank"> <i class='fab fa-linkedin linkedInLogo' style={{fontSize:'48px',color:'blue'}}></i>
+              <a href="https://www.linkedin.com/in/harsh-jha-457707203/" rel="noreferrer"
+                     target="_blank"> <FontAwesomeIcon icon={faLinkedin} className=' linkedInLogo' style={{fontSize:'48px',color:'blue'}} /> {/*<i class='fab fa-linkedin linkedInLogo' style={{fontSize:'48px',color:'blue'}}></i>*/}
                      {/* <img
                         className="linkedInLogo"
                         src="images/iconfinder_Linkedin_social_media_logo_1964405.svg"
                         alt="LinkedInLink" />*/} </a>
-                  <a href="https://www.instagram.com/_adarsh_shahi/" rel="noreferrer"
-                     target="_blank"><i class='fab fa-instagram instagramLogo' style={{fontSize:'48px',color:'orange'}}></i>
+                  <a href="https://www.instagram.com/_mr_h_a_r_s_h_j_h_a/" rel="noreferrer"
+                     target="_blank"> <FontAwesomeIcon icon={faInstagram} className=' instagramLogo' style={{fontSize:'48px',color:'orange'}} />
+                        {/*<i class='fab fa-instagram instagramLogo' style={{fontSize:'48px',color:'orange'}}></i>*/}
                      {/* <img
                         className="instagramLogo"
                         src="images/iconfinder_Instagram_social_media_logo_1964411.svg"
                         alt="InstagramLink"/>*/}</a>
-                  <a href="https://www.facebook.com/adarsh.shahi.1009" rel="noreferrer"
-                     target="_blank"><i class='fab fa-facebook-f facebookLogo' style={{fontSize:'48px',color:'blue'}}></i>
+                  <a href="https://www.facebook.com/harshkumar.jha.568/" rel="noreferrer"
+                     target="_blank"><FontAwesomeIcon icon={faFacebook} className=' facebookLogo' style={{fontSize:'48px',color:'blue'}} />
+                        {/*<i class='fab fa-facebook-f facebookLogo' style={{fontSize:'48px',color:'blue'}}></i>*/}
                          {/* <img
                         className="facebookLogo"
                         src="images/iconfinder_Facebook_social_media_logo_1964400.svg"
                         alt="FacebookLink" />*/} </a>
-                  <a href="https://twitter.com/_adarsh_shahi" rel="noreferrer"
-                     target="_blank"><i class='fab fa-twitter twitterLogo' style={{fontSize:'48px',color:'rgb(52 208 242)'}}></i>
+                  <a href="https://twitter.com/Harshku79928569" rel="noreferrer"
+                     target="_blank"><FontAwesomeIcon icon={faTwitter} className=' twitterLogo' style={{fontSize:'48px',color:'rgb(52 208 242)'}} />
+                        {/*<i class='fab fa-twitter twitterLogo' style={{fontSize:'48px',color:'rgb(52 208 242)'}}></i>*/}
                          {/* <img
                         className="twitterLogo"
                         src="images/iconfinder_Twitter_social_media_logo_1964397.svg"
                         alt="TwitterLink" />*/}</a>
-                  <a href="https://github.com/adarsh-shahi" rel="noreferrer"
-                     target="_blank"><i class='fab fa-github-alt githubLogo' style={{fontSize:'48px',color:'black'}}></i>
+                  <a href="https://github.com/mr-Harsh-Kumar-Jha" rel="noreferrer"
+                     target="_blank"><FontAwesomeIcon icon={faGithub} className=' githubLogo' style={{fontSize:'48px',color:'black'}} />
+                        {/* <i class='fab fa-github-alt githubLogo' style={{fontSize:'48px',color:'black'}}></i> */}
                      {/* <img
                         className="githubLogo"
                         src="images/iconfinder_Github_social_media_logo_1964417.svg"
--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact, faNode , faHtml5, faCss3 , faEnvira ,faCodiepie ,faCuttlefish ,faJsSquare } from '@fortawesome/free-brands-svg-icons';
+
 const TechStack = () => {
    return (
       <>
@@ -9,31 +12,38 @@ const TechStack = () => {
                </div>
                <div className='container techstack-maincontent1'>
                   <div className='row row-hover '>
-                     <div className='col'>
-                     <i class='fab fa-cuttlefish' style={{fontSize:'48px',color:'rgb(7 93 228)'}}> C </i>
+                     <div className='col'><FontAwesomeIcon icon={faCuttlefish} className=' facebookLogo' style={{fontSize:'48px',color:'rgb(7 93 228)'}} /><span style={{fontSize:'48px',color:'rgb(7 93 228)'}}> C</span>
+                     {/* <i class='fab fa-cuttlefish' style={{fontSize:'48px',color:'rgb(7 93 228)'}}> C </i> */}
                      </div>
                      <div className='col'>
-                     <i class='fab fa-codiepie' style={{fontSize:'48px',color:'rgb(90 90 231)'}}> C++ </i>
+                     <FontAwesomeIcon icon={faCodiepie} className=' facebookLogo' style={{fontSize:'48px',color:'rgb(90 90 231)'}} />  <span style={{fontSize:'48px',color:'rgb(90 90 231)'}}> C++</span>
+                     {/* <i class='fab fa-codiepie' style={{fontSize:'48px',color:'rgb(90 90 231)'}}> C++ </i> */}
                      </div>
                      <div className='col'>
-                     <i class='fab fa-html5' style={{fontSize:'48px',color:'red'}}> HTML</i>
+                     <FontAwesomeIcon icon={faHtml5} className=' facebookLogo' style={{fontSize:'48px',color:'red'}} />  <span style={{fontSize:'48px',color:'red'}}> HTML</span>
+                     {/* <i class='fab fa-html5' style={{fontSize:'48px',color:'red'}}> HTML</i> */}
                      </div>
                      <div className='col'>
-                     <i class='fab fa-css3' style={{fontSize:'48px',color:'rgb(76 153 238)'}}> CSS</i>
+                     <FontAwesomeIcon icon={faCss3} className=' facebookLogo' style={{fontSize:'48px',color:'rgb(76 153 238)'}} />  <span  style={{fontSize:'48px',color:'rgb(76 153 238)'}}> CSS</span>
+                     {/* <i class='fab fa-css3' style={{fontSize:'48px',color:'rgb(76 153 238)'}}> CSS</i> */}
                      </div>
                   </div>
                   <div className='row mt-5'>
                   <div className='col'>
-                     <i class='fab fa-js-square' style={{fontSize:'48px',color:'yellow'}}> JavaScript</i>
+                  <FontAwesomeIcon icon={faJsSquare} className=' facebookLogo' style={{fontSize:'48px',color:'yellow'}} />  <span style={{color:'yellow',fontSize:'48px'}}> JavaScript</span>
+                     {/* <i class='fab fa-js-square' style={{fontSize:'48px',color:'yellow'}}> JavaScript</i> */}
                      </div>
                      <div className='col'>
-                     <i class='fab fa-react' style={{fontSize:'48px',color:'rgb(112 240 230)'}}> React.js</i>
+                     <FontAwesomeIcon icon={faReact} className=' facebookLogo' style={{fontSize:'48px',color:'rgb(112 240 230)'}} />  <span style={{fontSize:'48px',color:'rgb(112 240 230)'}}> React.js</span>
+                     {/* <i class='fab fa-react' style={{fontSize:'48px',color:'rgb(112 240 230)'}}> React.js</i> */}
                      </div>
                      <div className='col'>
-                     <i class='fab fa-node' style={{fontSize:'48px',color:'green'}}> Node.js</i>
+                     <FontAwesomeIcon icon={faNode} className=' facebookLogo' style={{fontSize:'48px',color:'green'}} />  <span style={{fontSize:'48px',color:'green'}}> Node.js</span>
+                     {/* <i class='fab fa-node' style={{fontSize:'48px',color:'green'}}> Node.js</i> */}
                      </div>
                      <div className='col'>
-                     <i class='fab fa-envira' style={{fontSize:'48px',color:'rgb(57 231 57)'}}> MongoDB</i>
+                     <FontAwesomeIcon icon={faEnvira} className=' facebookLogo' style={{fontSize:'48px',color:'rgb(57 231 57)'}} />  <span style={{fontSize:'48px',color:'rgb(57 231 57)'}}> MongoDB </span>
+                     {/* <i class='fab fa-envira' style={{fontSize:'48px',color:'rgb(57 231 57)'}}> MongoDB</i> */}
                      </div>
                   </div>
                </div>
